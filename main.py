1679e6ba1eda23ccf470dea08c24e6c172697b49
luizpgt <luiz.paulus51@gmail.com>
2022-04-30 03:41:11 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,85 @@
+def make_set():
+    word_list = []
+    line_break = '\n'
+    with open("lista-palavras.txt") as palavras:
+        for line in palavras.readlines():
+            word_list.append(line.replace(line_break, '').lower())
+    word_set = set(word_list)
+    return word_set
+
+def print_set(set):
+    for word in set:
+        print("Word : {}\n".format(list(word)))
+
+def get_contained_letters(word_set): 
+    known_letters = input("Enter the known letters in the word: ")
+    return filtered_by_contained_letters(list(known_letters), word_set)
+
+def filtered_by_contained_letters(known_letters, word_set):
+    # known_letters is a list of char
+    result_list = []
+    for word in word_set:
+        present_letter_count = 0
+        for letter in known_letters:
+            if letter in word:
+                present_letter_count += 1
+            if present_letter_count == len(known_letters):
+                result_list.append(word)
+    result_set = set(result_list)
+    return result_set
+
+def get_not_in_word_letters(word_set):
+    known_not_contained_letters = input("Enter the known letters not in the word: ")
+    return filtered_by_not_in_word_letters(known_not_contained_letters, word_set)
+
+def filtered_by_not_in_word_letters(known_not_in, word_set):
+    print("")
+
+def get_right_placed_letters(word_set):
+    # wildcard = @
+    print("Example: \nAUDIO \n@B@@A")
+    known_part_of_word = input("Enter the right placed letters you already have: ")
+    return filtered_by_right_placed_letters(known_part_of_word, word_set)
+
+def filtered_by_right_placed_letters(known_part, word_set):
+    # known_part is a string
+    wildcard = '@'
+    result_list = []
+    
+    known_chars_no_wildcards = ''.join(filter(lambda char: char != wildcard, known_part))
+
+    for word in word_set:
+        right_placed_letters_count = 0
+        for char in known_part:
+            if char != '@':
+                if known_part.index(char) == word.index(char):
+                    right_placed_letters_count += 1
+                if right_placed_letters_count == len(known_chars_no_wildcards):
+                    result_list.append(word)
+    result_set = set(result_list)
+    return result_set  
+        
+def get_wrong_placed_letters():
+    print("")
+
+def filtered_by_wrong_placed_letters():
+    print("")
+
+def search_result():
+    print('teste')
+
+def write_result(resulted_word_set):
+    result_file = open("result-file.txt", "w")
+    for word in resulted_word_set:
+        word += '\n'
+        result_file.writelines(word)
+    
+if __name__ == "__main__":
+    initial_word_set = make_set()
+    #print_set(word_set)
+    contained_letters_word_set = get_contained_letters(initial_word_set)
+    right_placed_letters_word_set = get_right_placed_letters(contained_letters_word_set)
+    print_set(right_placed_letters_word_set)
+    write_result(right_placed_letters_word_set)
+    #right_placed_letters_word_set = get_right_placed_letters(word_set)
+    
